e0d68cffa5ac8e4d89b87e22df7c9d4e7b689466
rare-wolf <gabeduncan018@gmail.com>
2024-09-23 19:27:47 +0600

    remove error file

--- src/components/HomeComponents/products/LoadMoreButton.tsx
+++ src/components/HomeComponents/products/LoadMoreButton.tsx
@@ -1,26 +1,25 @@
+// "use client"
 
-"use client"
+// import { useState } from "react"
+// import { Button } from "@/components/ui/button"
+// import Products from "./Products"
 
-import { useState } from "react"
-import { Button } from "@/components/ui/button"
-import Products from "./Products"
+// export default function LoadMoreProducts() {
+//   const [visibleProducts, setVisibleProducts] = useState(4)
 
-export default function LoadMoreProducts() {
-  const [visibleProducts, setVisibleProducts] = useState(4)
+//   const handleLoadMore = () => {
+//     setVisibleProducts(visibleProducts + 4)
+//   }
 
-  const handleLoadMore = () => {
-    setVisibleProducts(visibleProducts + 4)
-  }
+//   return (
+//     <div>
+//       {/* Server component rendering products */}
+//       <Products limit={visibleProducts} />
 
-  return (
-    <div>
-      {/* Server component rendering products */}
-      <Products limit={visibleProducts} />
-
-      {/* Load more button */}
-      <div className="mt-8 text-center">
-        <Button onClick={handleLoadMore}>Load More</Button>
-      </div>
-    </div>
-  )
-}
+//       {/* Load more button */}
+//       <div className="mt-8 text-center">
+//         <Button onClick={handleLoadMore}>Load More</Button>
+//       </div>
+//     </div>
+//   )
+// }
